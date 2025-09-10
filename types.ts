1aea56e90d066531945592a8d1f38910f8cf4345
aurelique <aurelique.perfumery@gmail.com>
2025-09-10 15:40:48 +0700

    create types.ts

---
+++ types.ts
@@ -0,0 +1,31 @@
+
+export enum FragranceFamily {
+  Woody = 'Woody',
+  Citrus = 'Citrus',
+  Oriental = 'Oriental',
+  Floral = 'Floral',
+  Spicy = 'Spicy',
+  Fresh = 'Fresh',
+  Aromatic = 'Aromatic',
+}
+
+export enum Usage {
+  Day = 'Day',
+  Night = 'Night',
+  AllSeason = 'All Season',
+  SpecialOccasion = 'Special Occasion',
+}
+
+export interface Perfume {
+  id: number;
+  name: string;
+  tagline: string;
+  mainChords: FragranceFamily[];
+  topNotes: string[];
+  heartNotes: string[];
+  baseNotes: string[];
+  usage: Usage;
+  price: number;
+  isBestSeller: boolean;
+  image: string;
+}
